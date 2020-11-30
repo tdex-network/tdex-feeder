d218afca3487525ea2f6fc16d4c0fb444c326d0b
louisinger <louis@vulpem.com>
2020-11-30 10:53:29 +0100

    ports layer kraken web socket

---
+++ internal/ports/krakenWebSocket.go
@@ -0,0 +1,109 @@
+package ports
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"strconv"
+
+	"github.com/gorilla/websocket"
+)
+
+type KrakenWebSocket interface {
+	Connect(address string, tickersToSubscribe []string) error
+	Read() (*TickerWithPrice, error)
+	Close() error
+}
+
+type krakenWebSocket struct {
+	connSocket *websocket.Conn
+}
+
+func NewKrakenWebSocket() KrakenWebSocket {
+	return &krakenWebSocket{
+		connSocket: nil,
+	}
+}
+
+func (socket *krakenWebSocket) Connect(address string, tickersToSubscribe []string) error {
+	conn, err := connectToSocket(address)
+	if err != nil {
+		return err
+	}
+
+	for _, ticker := range tickersToSubscribe {
+		msg := createSubscribeToMarketMessage(ticker)
+		msgBytes, err := json.Marshal(msg)
+		if err != nil {
+			return err
+		}
+
+		err = conn.WriteMessage(websocket.TextMessage, msgBytes)
+		if err != nil {
+			return err
+		}
+	}
+
+	socket.connSocket = conn
+
+	return nil
+}
+
+func (socket *krakenWebSocket) Close() error {
+	err := socket.connSocket.Close()
+	return err
+}
+
+func (socket *krakenWebSocket) Read() (*TickerWithPrice, error) {
+	if socket.connSocket == nil {
+		return nil, errors.New("Socket not connected")
+	}
+
+	var msgAsJson []interface{}
+
+	_, message, err := socket.connSocket.ReadMessage()
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(message), &msgAsJson)	
+	if err != nil {
+		return nil, err
+	}
+
+	if len(msgAsJson) < 4 {
+		return nil, errors.New("Invalid message" + fmt.Sprint(message))
+	}
+
+	pricesJson := msgAsJson[1].(map[string]interface{})
+	priceAsk := pricesJson["c"].([]interface{})
+	price, _ := strconv.ParseFloat(priceAsk[0].(string), 64)
+
+	ticker := msgAsJson[3].(string)
+
+	return &TickerWithPrice{
+		ticker: ticker,
+		price: price,
+	}, nil
+}
+
+// ConnectToSocket dials and returns a new client connection to a remote host
+func connectToSocket(address string) (*websocket.Conn, error) {
+	u := url.URL{Scheme: "wss", Host: address, Path: "/"}
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return c, err
+	}
+	log.Println("Connected to socket:", u.String())
+	return c, nil
+}
+
+// CreateSubscribeToMarketMessage gets a string with a market pair and returns
+// a RequestMessage struct with instructions to subscrive to that market pair ticker.
+func createSubscribeToMarketMessage(ticker string) requestMessage {
+	s := subscription{Name: "ticker"}
+	return requestMessage{"subscribe", []string{ticker}, &s, 0}
+}
