4195cc6a6159f2896f239be6a0b538651590190f
louisinger <louis@vulpem.com>
2020-12-01 08:52:04 +0100

    feeder service base implementation

--- internal/application/feeder_service.go
+++ internal/application/feeder_service.go
@@ -1,6 +1,9 @@
 package application
 
-import "github.com/tdex-network/tdex-feeder/internal/domain"
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/tdex-network/tdex-feeder/internal/domain"
+)
 
 type FeederService interface {
 	Start() error
@@ -9,13 +12,38 @@ type FeederService interface {
 
 type feederService struct {
 	tdexFeeder domain.TdexFeeder
-	krakenService krakenFeedService
+	krakenService FeedService
+}
+
+type NewFeederServiceArgs struct {
+	OperatorEndpoint string
+	KrakenWSaddress string
+	TickerToMarket map[string]domain.Market
+}
+
+func NewFeederService(args NewFeederServiceArgs) FeederService {
+	target := NewTdexDaemonTarget(args.OperatorEndpoint)
+	krakenFeedService, err := NewKrakenFeedService(args.KrakenWSaddress, args.TickerToMarket)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	feeder := domain.NewTdexFeeder([]domain.Feed{krakenFeedService.GetFeed()}, []domain.Target{target})
+
+	return &feederService{
+		tdexFeeder: feeder,
+		krakenService: krakenFeedService,
+	}
 }
 
 func (feeder *feederService) Start() error {
-
+	go feeder.krakenService.Start()
+	err := feeder.tdexFeeder.Start()
+	return err
 }
 
 func (feeder *feederService) Stop() error {
-	
+	feeder.krakenService.Stop()
+	feeder.tdexFeeder.Stop()
+	return nil	
 }
