ec86e1aa7c1dcadc9b1d11bbb6cafe3038571862
Pietralberto Mazza <pietro@vulpem.com>
2021-11-08 14:50:18 +0100

    Bitfinex price feeder (#40)
    * Add bitfinex price feeder
    
    * Explicitly support bitfinex feeder
    
    * Check for successful subscription
    
    * Fix parsing messages from bitfinex

---
+++ internal/core/infrastructure/feeder/bitfinex/types.go
@@ -0,0 +1,49 @@
+package bitfinexfeeder
+
+import (
+	"github.com/tdex-network/tdex-feeder/internal/core/ports"
+)
+
+type price struct {
+	basePrice  string
+	quotePrice string
+}
+
+func (p *price) BasePrice() string {
+	return p.basePrice
+}
+
+func (p *price) QuotePrice() string {
+	return p.quotePrice
+}
+
+type priceFeed struct {
+	market ports.Market
+	price  *price
+}
+
+func (p *priceFeed) GetMarket() ports.Market {
+	return p.market
+}
+
+func (p *priceFeed) GetPrice() ports.Price {
+	return p.price
+}
+
+type market struct {
+	baseAsset  string
+	quoteAsset string
+	ticker     string
+}
+
+func (m market) BaseAsset() string {
+	return m.baseAsset
+}
+
+func (m market) QuoteAsset() string {
+	return m.quoteAsset
+}
+
+func (m market) Ticker() string {
+	return m.ticker
+}
